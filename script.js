1216023048a7a7b2f3412034fb0e6ac01ba594d0
Daniel <dselasea@gmail.com>
2022-10-09 21:12:20 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let compScore = 0
+let manScore = 0
+
+const computerSelection = function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"]
+    const compChoice = Math.floor(Math.random() * 3)
+    return choices[compChoice]
+}
+
+const playerSelection = function getManChoice(){
+    let manSelection = prompt("Enter your choice!")
+    let lManSelection = manSelection.toLowerCase()
+    checkAnswer(lManSelection)
+    return lManSelection
+   
+}
+const count = function(){
+    console.log(manScore, compScore)
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
+        manScore++ 
+        return count()
+    }
+    if(playerSelection == "scissors" && computerSelection == "rock"){
+        console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
+        compScore++
+        return count()
+    }
+    if(playerSelection == "paper" && computerSelection == "rock"){
+        console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
+        manScore++
+        return count()
+    }
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
+        compScore++
+        return count()
+    }
+    if(playerSelection == "scissors" && computerSelection == "paper"){
+        console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
+        manScore++
+        return count()
+    }
+    if(playerSelection == "paper" && computerSelection == "scissors"){
+        console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
+        compScore++
+        return count()
+    }
+    if(playerSelection == computerSelection){
+        console.log("It's a tie!")
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        playRound(playerSelection(), computerSelection())
+    }
+}
+
+function checkAnswer(manAnswer){
+    if(manAnswer !== "rock"  && manAnswer !=="paper" && manAnswer !== "scissors"){
+        console.log("Wrong input!")
+    }
+}
+
+game()
