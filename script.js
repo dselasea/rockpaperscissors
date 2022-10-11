4fb1b8273d36f00856b285fee3fb51afbfe83f2b
Daniel <dselasea@gmail.com>
2022-10-11 23:29:54 +0000

    added a play again button and linked it to the html

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const playerChoice = document.querySelectorAll(".game-board_player button")
 const computerChoice = document.querySelector(".game-board_computer button")
 const score = document.querySelector("#score")
+const cscore = document.querySelector("#cscore")
+const announcer = document.querySelector(".announcer")
+const play = document.querySelector("#play-again")
 
 let compScore = 0
 let manScore = 0
 let playerSelection;
 
-/*==============================================
-    This function generates computer choice
-==============================================*/
+playerChoice.forEach(choice => {
+    choice.addEventListener("click", () => {
+        playerSelection = choice.id
+        playRound(playerSelection, computerSelection())
+        score.textContent = manScore
+        cscore.textContent = compScore
+        scoreToFive()
+    })
+})
+
+
 const computerSelection = function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"]
     const compChoice = Math.floor(Math.random() * 3)
@@ -16,39 +27,25 @@ const computerSelection = function getComputerChoice(){
     return choices[compChoice]
 }
 
+const scoreToFive = function(){
+    if(manScore == 5){
+        announcer.style.display = "block"
+        announcer.firstElementChild.textContent = "You Win!"
+    }
+    if(compScore == 5){
+        announcer.style.display = "block"
+        announcer.firstElementChild.textContent = "Computer Wins!"
+    }
+}
 
-playerChoice.forEach(choice => {
-    choice.addEventListener("click", () => {
-        playerSelection = choice.id
-        playRound(playerSelection, computerSelection())
-    })
-})
+play.addEventListener("click", playAgain)
 
-score.textContent = manScore
-
-
-
-/*==============================================
-    This function asks user to select choice
-==============================================*/
-// const playerSelection = function getManChoice(){
-//         let manSelection = prompt("Enter your choice!")
-//         if(manSelection === ""){
-//             console.log("Empty")
-//         }else if(manSelection){
-//             let lManSelection = manSelection.toLowerCase()
-//             checkAnswer(lManSelection)
-//             return lManSelection
-//         }else{
-//             console.log("You hit cancel")
-//         }
-// }
-
-/*==============================================
-    This function prints the scores
-==============================================*/
-const count = function(){
-    console.log(manScore, compScore)
+function playAgain(){
+    announcer.style.display = "none"
+    compScore = 0
+    manScore = 0
+    score.textContent = "0"
+    cscore.textContent = "0"
 }
 
 
@@ -59,32 +56,26 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection == "rock" && computerSelection == "scissors"){
         console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
         manScore++ 
-        return count()
     }
     if(playerSelection == "scissors" && computerSelection == "rock"){
         console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
         compScore++
-        return count()
     }
     if(playerSelection == "paper" && computerSelection == "rock"){
         console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
         manScore++
-        return count()
     }
     if(playerSelection == "rock" && computerSelection == "paper"){
         console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
         compScore++
-        return count()
     }
     if(playerSelection == "scissors" && computerSelection == "paper"){
         console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
         manScore++
-        return count()
     }
     if(playerSelection == "paper" && computerSelection == "scissors"){
         console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
         compScore++
-        return count()
     }
     if(playerSelection == computerSelection){
         console.log("It's a tie!")
